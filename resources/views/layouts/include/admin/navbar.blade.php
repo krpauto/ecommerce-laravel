1c050a502c248b30eca665e685730f14e7167f29
Irvansyah <ilhamirvansyah.dev@gmail.com>
2024-06-17 10:31:17 +0700

    admin - logout

--- resources/views/layouts/include/admin/navbar.blade.php
+++ resources/views/layouts/include/admin/navbar.blade.php
@@ -18,39 +18,30 @@
         <li class="dropdown user user-menu">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
             <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
-            <span class="hidden-xs">Alexander Pierce</span>
+            <span class="hidden-xs">{{ Auth::user()->name }}</span>
           </a>
-          <ul class="dropdown-menu">  
+          <ul class="dropdown-menu">
             <!-- User image -->
             <li class="user-header">
               <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
 
               <p>
-                Alexander Pierce - Web Developer
-                <small>Member since Nov. 2012</small>
+                {{ Auth::user()->name }}
+                <small>Member since {{ Auth::user()->created_at->format('M. Y') }}</small>
               </p>
             </li>
 
-            <li class="user-body">
-              <div class="row">
-                <div class="col-xs-4 text-center">
-                  <a href="#">Followers</a>
-                </div>
-                <div class="col-xs-4 text-center">
-                  <a href="#">Sales</a>
-                </div>
-                <div class="col-xs-4 text-center">
-                  <a href="#">Friends</a>
-                </div>
-              </div>
-            </li>
-
             <li class="user-footer">
-              <div class="pull-left">
-                <a href="#" class="btn btn-default btn-flat">Profile</a>
-              </div>
-              <div class="pull-right">
-                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+              <div class="">
+                {{-- <a href="#" class="btn btn-default btn-block btn-flat">Logout</a> --}}
+                <a class="dropdown-item btn btn-default btn-block btn-flat" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+                </form>
               </div>
             </li>
 
