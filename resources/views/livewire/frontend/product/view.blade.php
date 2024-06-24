272e728ba36a4c4c6759f70bdad083a79f0d39ba
Irvansyah <ilhamirvansyah.dev@gmail.com>
2024-06-24 14:35:22 +0700

    frontend - view product

---
+++ resources/views/livewire/frontend/product/view.blade.php
@@ -0,0 +1,68 @@
+<div>
+    <div class="py-3 py-md-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 mt-3">
+                    <div class="bg-white border">
+                        @if ($product->productImages)
+                        <img src="{{ asset($product->productImages[0]->image) }}" class="w-100" alt="Img">
+                        @else
+                        No Image Added
+                        @endif
+                    </div>
+                </div>
+                <div class="col-md-7 mt-3">
+                    <div class="product-view">
+                        <h4 class="product-name">
+                            {{ $product->name }}
+                            <label class="label-stock bg-success">In Stock</label>
+                        </h4>
+                        <hr>
+                        <p class="product-path">
+                            Home / {{ $product->category->name }} / {{ $product->name }}
+                        </p>
+                        <div>
+                            <span class="selling-price">Rp {{ number_format($product->selling_price, 0, ',',
+                                '.')
+                                }}</span>
+                            <span class="original-price">Rp {{ number_format($product->original_price, 0, ',',
+                                '.')
+                                }}</span>
+                        </div>
+                        <div class="mt-2">
+                            <div class="input-group">
+                                <span class="btn btn1"><i class="fa fa-minus"></i></span>
+                                <input type="text" value="1" class="input-quantity" />
+                                <span class="btn btn1"><i class="fa fa-plus"></i></span>
+                            </div>
+                        </div>
+                        <div class="mt-2">
+                            <a href="" class="btn btn1"> <i class="fa fa-shopping-cart"></i> Add To Cart</a>
+                            <a href="" class="btn btn1"> <i class="fa fa-heart"></i> Add To Wishlist </a>
+                        </div>
+                        <div class="mt-3">
+                            <h5 class="mb-0">Small Description</h5>
+                            <p>
+                                {!! $product->small_description !!}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 mt-3">
+                    <div class="card">
+                        <div class="card-header bg-white">
+                            <h4>Description</h4>
+                        </div>
+                        <div class="card-body">
+                            <p>
+                                {!! $product->description !!}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
