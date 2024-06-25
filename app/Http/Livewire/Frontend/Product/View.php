f25df432e7d276dc022e415f50615adf75fd36f3
Irvansyah <ilhamirvansyah.dev@gmail.com>
2024-06-25 20:44:07 +0700

    frontend - wishlist product

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -3,10 +3,32 @@
 namespace App\Http\Livewire\Frontend\Product;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Wishlist;
 
 class View extends Component
 {
     public $product, $category;
+
+    public function addToWishlist($productId)
+    {
+        if (Auth::check()) {
+
+            if (Wishlist::where('user_id', auth()->user()->id)->where('product_id', $productId)->exists()) {
+                session()->flash('message', 'Already added to wishlist');
+                return false;
+            } else {
+                Wishlist::create([
+                    'user_id' => auth()->user()->id,
+                    'product_id' => $productId
+                ]);
+                session()->flash('message', 'Wishlist Added to wishlist');
+            }
+        } else {
+            session()->flash('message', 'Please Login to Continue');
+            return false;
+        }
+    }
 
     public function mount($category, $product)
     {
--- resources/views/livewire/frontend/product/view.blade.php
+++ resources/views/livewire/frontend/product/view.blade.php
@@ -1,6 +1,12 @@
 <div>
     <div class="py-3 py-md-5 bg-light">
         <div class="container">
+            @if (session()->has('message'))
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                {{ session('message') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
             <div class="row">
                 <div class="col-md-5 mt-3">
                     <div class="bg-white border">
@@ -42,7 +48,8 @@
                         </div>
                         <div class="mt-2">
                             <a href="" class="btn btn1"> <i class="fa fa-shopping-cart"></i> Add To Cart</a>
-                            <a href="" class="btn btn1"> <i class="fa fa-heart"></i> Add To Wishlist </a>
+                            <button type="button" wire:click="addToWishlist({{ $product->id }})" class="btn btn1"> <i
+                                    class="fa fa-heart"></i> Add To Wishlist </button>
                         </div>
                         <div class="mt-3">
                             <h5 class="mb-0">Small Description</h5>
