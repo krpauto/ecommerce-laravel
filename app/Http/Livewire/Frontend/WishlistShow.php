79c181f5794df5f220023fb2ba7b39284ae91820
Irvansyah <ilhamirvansyah.dev@gmail.com>
2024-06-30 13:48:01 +0700

    frontend - wishlist remove

---
+++ app/Http/Livewire/Frontend/WishlistShow.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire\Frontend;
+
+use Livewire\Component;
+use App\Models\Wishlist;
+
+class WishlistShow extends Component
+{
+    public function removeWishlistItem(int $wishlistId)
+    {
+        // dd($wishlistId);
+        Wishlist::where('user_id', auth()->user()->id)->where('id', $wishlistId)->delete();
+        $this->dispatchBrowserEvent('wishlist-updated', [
+            'message' => 'Wishlist Item Removed Successfully',
+            'type' => 'success'
+        ]);
+    }
+
+    public function render()
+    {
+        $wishlist = Wishlist::where('user_id', auth()->user()->id)->get();
+        return view('livewire.frontend.wishlist-show', [
+            'wishlist' => $wishlist
+        ]);
+    }
+}
