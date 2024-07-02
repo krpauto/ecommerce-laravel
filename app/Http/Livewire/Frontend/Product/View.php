e7b7e5d19f0300ba60deda71ad65eb30fdccc968
Irvansyah <ilhamirvansyah.dev@gmail.com>
2024-07-02 15:36:22 +0700

    frontend - product add cart

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Frontend\Product;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Wishlist;
+use App\Models\Cart;
 
 class View extends Component
 {
@@ -57,6 +58,115 @@ class View extends Component
             $this->quantityCount--;
         }
     }
+
+    // public function addToCart(int $productId)
+    // {
+    //     if (Auth::check()) {
+    //         // dd($productId);
+    //         if ($this->product->where('id', $productId)->where('status', '0')->exists()) {
+
+    //             if (Cart::where('user_id', auth()->user()->id)->where('product_id', $productId)->exists()) {
+    //                 $this->dispatchBrowserEvent('wishlist-updated', [
+    //                     'message' => 'Product already in cart',
+    //                     'type' => 'warning'
+    //                 ]);
+
+    //             } else {
+
+    //             if ($this->product->quantity > 0) {
+
+    //                 if ($this->product->quantity > $this->quantityCount) {
+    //                     // Insert Product to Cart
+    //                     Cart::create([
+    //                         'user_id' => auth()->user()->id,
+    //                         'product_id' => $productId,
+    //                         'quantity'   => $this->quantityCount
+    //                     ]);
+
+    //                     $this->dispatchBrowserEvent('wishlist-updated', [
+    //                         'message' => 'Product Added to cart',
+    //                         'type' => 'success'
+    //                     ]);
+    //                 } else {
+    //                     $this->dispatchBrowserEvent('wishlist-updated', [
+    //                         'message' => 'Product ' . $this->product->quantity . 'Quantity Available',
+    //                         'type' => 'warning'
+    //                     ]);
+    //                 }
+    //             } else {
+    //                 $this->dispatchBrowserEvent('wishlist-updated', [
+    //                     'message' => 'Product out of stock',
+    //                     'type' => 'warning'
+    //                 ]);
+    //             }
+    //         } else {
+    //             $this->dispatchBrowserEvent('wishlist-updated', [
+    //                 'message' => 'Product does not exists',
+    //                 'type' => 'warning'
+    //             ]);
+    //         }
+    //     }
+    //     } else {
+    //         $this->dispatchBrowserEvent('wishlist-updated', [
+    //             'message' => 'Please login to add to cart',
+    //             'type' => 'warning'
+    //         ]);
+    //     }
+    // }
+
+    public function addToCart(int $productId)
+    {
+        if (Auth::check()) {
+            // dd($productId);
+            $product = $this->product->where('id', $productId)->where('status', '0')->first();
+
+            if ($product) {
+                if (Cart::where('user_id', auth()->user()->id)->where('product_id', $productId)->exists()) {
+                    $this->dispatchBrowserEvent('wishlist-updated', [
+                        'message' => 'Product already in cart',
+                        'type' => 'warning'
+                    ]);
+                } else {
+                    if ($product->quantity > 0) {
+                        if ($product->quantity > $this->quantityCount) {
+                            // Insert Product to Cart
+                            Cart::create([
+                                'user_id' => auth()->user()->id,
+                                'product_id' => $productId,
+                                'quantity' => $this->quantityCount
+                            ]);
+
+                            $this->dispatchBrowserEvent('wishlist-updated', [
+                                'message' => 'Product added to cart',
+                                'type' => 'success'
+                            ]);
+                        } else {
+                            $this->dispatchBrowserEvent('wishlist-updated', [
+                                'message' => 'Only ' . $product->quantity . ' quantity available',
+                                'type' => 'warning'
+                            ]);
+                        }
+                    } else {
+                        $this->dispatchBrowserEvent('wishlist-updated', [
+                            'message' => 'Product out of stock',
+                            'type' => 'warning'
+                        ]);
+                    }
+                }
+            } else {
+                $this->dispatchBrowserEvent('wishlist-updated', [
+                    'message' => 'Product does not exist',
+                    'type' => 'warning'
+                ]);
+            }
+        } else {
+            $this->dispatchBrowserEvent('wishlist-updated', [
+                'message' => 'Please login to add to cart',
+                'type' => 'warning'
+            ]);
+        }
+    }
+
 
     public function mount($category, $product)
     {
--- resources/views/livewire/frontend/product/view.blade.php
+++ resources/views/livewire/frontend/product/view.blade.php
@@ -48,7 +48,11 @@
                             </div>
                         </div>
                         <div class="mt-2">
-                            <a href="" class="btn btn1"> <i class="fa fa-shopping-cart"></i> Add To Cart</a>
+
+                            <button type="button" wire:click="addToCart({{ $product->id }})" class="btn btn1">
+                                <i class="fa fa-shopping-cart"></i> Add To Cart
+                            </button>
+
                             <button type="button" wire:click="addToWishlist({{ $product->id }})" class="btn btn1">
                                 <span wire:loading.remove wire:targer="addToWishlist">
                                     <i class="fa fa-heart"></i> Add To Wishlist
