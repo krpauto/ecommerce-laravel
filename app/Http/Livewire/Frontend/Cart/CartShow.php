eb483691d05694e2f1a6e11f36292b8f447199f9
Irvansyah <ilhamirvansyah.dev@gmail.com>
2024-07-05 17:44:54 +0700

    frontend - cart item quantity

--- app/Http/Livewire/Frontend/Cart/CartShow.php
+++ app/Http/Livewire/Frontend/Cart/CartShow.php
@@ -8,6 +8,55 @@ use App\Models\Cart;
 class CartShow extends Component
 {
     public $cart;
+
+    public function decrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id', $cartId)->where('user_id', auth()->user()->id)->first();
+        if ($cartData) {
+            if ($cartData->product->quantity > $cartData->quantity) {
+                $cartData->decrement('quantity');
+                $this->dispatchBrowserEvent('wishlist-updated', [
+                    'message' => 'Quantity Updated',
+                    'type' => 'success'
+                ]);
+            } else {
+                $this->dispatchBrowserEvent('wishlist-updated', [
+                    'message' => 'Only ' . $cartData->product->quantity . ' quantity available',
+                    'type' => 'error'
+                ]);
+            }
+        } else {
+            $this->dispatchBrowserEvent('wishlist-updated', [
+                'message' => 'Something went wrong',
+                'type' => 'error'
+            ]);
+        }
+    }
+
+    public function incrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id', $cartId)->where('user_id', auth()->user()->id)->first();
+        if ($cartData) {
+            if ($cartData->product->quantity > $cartData->quantity) {
+                $cartData->increment('quantity');
+                $this->dispatchBrowserEvent('wishlist-updated', [
+                    'message' => 'Quantity Updated',
+                    'type' => 'success'
+                ]);
+            } else {
+                $this->dispatchBrowserEvent('wishlist-updated', [
+                    'message' => 'Only ' . $cartData->product->quantity . ' quantity available',
+                    'type' => 'error'
+                ]);
+            }
+        } else {
+            $this->dispatchBrowserEvent('wishlist-updated', [
+                'message' => 'Something went wrong',
+                'type' => 'error'
+            ]);
+        }
+    }
+
     public function render()
     {
         $this->cart = Cart::where('user_id', auth()->user()->id)->get();
--- resources/views/livewire/frontend/cart/cart-show.blade.php
+++ resources/views/livewire/frontend/cart/cart-show.blade.php
@@ -11,7 +11,7 @@
 
                         <div class="cart-header d-none d-sm-none d-mb-block d-lg-block">
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-5">
                                     <h4>Products</h4>
                                 </div>
                                 <div class="col-md-2">
@@ -21,6 +21,9 @@
                                     <h4>Quantity</h4>
                                 </div>
                                 <div class="col-md-2">
+                                    <h4>Total</h4>
+                                </div>
+                                <div class="col-md-1">
                                     <h4>Remove</h4>
                                 </div>
                             </div>
@@ -30,7 +33,7 @@
                         @if ($cartItem->product)
                         <div class="cart-item">
                             <div class="row">
-                                <div class="col-md-6 my-auto">
+                                <div class="col-md-5 my-auto">
                                     <a
                                         href="{{ url('collections/' . $cartItem->product?->category?->slug . '/' . $cartItem->product?->slug) }}">
                                         <label class="product-name">
@@ -54,14 +57,29 @@
                                 <div class="col-md-2 col-7 my-auto">
                                     <div class="quantity">
                                         <div class="input-group">
-                                            <span class="btn btn1"><i class="fa fa-minus"></i></span>
+
+                                            <button type="button" wire:loading.attr="disabled"
+                                                wire:click="decrementQuantity({{ $cartItem->id }})" class="btn btn1"><i
+                                                    class="fa fa-minus"></i></button>
+
                                             <input type="text" value="{{ $cartItem->quantity }}"
                                                 class="input-quantity" />
-                                            <span class="btn btn1"><i class="fa fa-plus"></i></span>
+
+                                            <button type="button" wire:loading.attr="disabled"
+                                                wire:click="incrementQuantity({{ $cartItem->id }})" class="btn btn1"><i
+                                                    class="fa fa-plus"></i></button>
+
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-2 col-5 my-auto">
+                                <div class="col-md-2 my-auto">
+                                    @php
+                                    $totalPrice = $cartItem->product->selling_price * $cartItem->quantity;
+                                    @endphp
+                                    <label class="price">Rp {{ number_format($totalPrice, 0, ',', '.') }}</label>
+                                </div>
+
+                                <div class="col-md-1 col-5 my-auto">
                                     <div class="remove">
                                         <a href="" class="btn btn-danger btn-sm">
                                             <i class="fa fa-trash"></i> Remove
