3053cf15c38081ec4e6e6cccb70e3737f2b5bb90
Irvansyah <ilhamirvansyah.dev@gmail.com>
2024-07-09 13:57:43 +0700

    frontend - checkout cart

---
+++ resources/views/frontend/thank-you.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'Thank You For Your Order')
+
+@section('content')
+
+<div class="py-3 pyt-md-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <div class="p-4 shadow bg-white">
+          <h2>Logo</h2>
+          <h4>Thank you for your order</h4>
+          <a href="{{ url('collections') }}" class="btn btn-primary">Shop Now</a>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
